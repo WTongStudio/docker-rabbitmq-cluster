a104cdefc09a4fc79bc6eb1581c0ff5684d7b935
wutong <wutong@cmcm.com>
2021-05-09 09:30:47 +0800

    * up fixbug

--- cluster-entrypoint.sh
+++ cluster-entrypoint.sh
@@ -7,8 +7,12 @@ set -e
 # will be honored
 /usr/local/bin/docker-entrypoint.sh rabbitmq-server -detached
 
+# Wait a while for RMQ really
+sleep 20s # 此处非常重要，若直接执行下面命令，会报错：rabbitmq 未运行
+
 # Do the cluster dance
 rabbitmqctl stop_app
+rabbitmqctl reset
 rabbitmqctl join_cluster rabbit@rabbitmq1
 
 # Stop the entire RMQ server. This is done so that we
